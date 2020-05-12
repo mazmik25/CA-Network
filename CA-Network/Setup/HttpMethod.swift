34f6fcf2319ae03bb340e31629c84480ddd628d5
mazmik25 <m.azmi65@yahoo.com>
2020-05-12 23:41:16 +0700

    adding httpmethod

---
+++ CA-Network/Setup/HttpMethod.swift
@@ -0,0 +1,20 @@
+//
+//  HttpMethod.swift
+//  CA-Network
+//
+//  Created by Azmi Muhammad on 12/05/20.
+//  Copyright © 2020 Clapping Ape. All rights reserved.
+//
+
+import Foundation
+public enum HttpMethod: String {
+    case GET
+    case HEAD
+    case POST
+    case PUT
+    case DELETE
+    case CONNECT
+    case OPTIONS
+    case TRACE
+    case PATCH
+}
